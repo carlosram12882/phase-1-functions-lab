a941e6f13b2c04205c2473a7d507d22008a041f8
Carlos Ramirez <carlosramirez686@gmail.com>
2022-01-02 14:12:19 -0800

    commit tree fba21f41e3e21e16e31253498d37b086fee0ebc0

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ function calculatesFarePrice(start, destination) {
     if (fare < 400) {
         return 0
     }
-    if (fare > 2000) {
+    if (fare > 400 && fare < 2000) {
+        return ((fare - 400) * 0.02)
+    }
+    if (fare > 2000 && fare < 2500) {
         return 25
     }
     if (fare > 2500) {
